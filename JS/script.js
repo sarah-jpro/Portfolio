e19bb2d3460021846b02837b5e85d626e2732915
Sarah <sarahjonet.pro@gmail.com>
2022-06-29 18:06:33 +0200

    portfolio

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// Typing Animation
+
+let typed = new Typed(".typing",{
+    strings:["Web Junior Developer","Frontend Developer","interested in UI/UX design"],
+    typeSpeed:150,
+    BackSpeed:70,
+    loop:true
+})
